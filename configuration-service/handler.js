f9f987b6b31080fe9c9db9cd487462fd3673c220
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-05-09 16:02:54 +0530

    add

--- configuration-service/handler.js
+++ configuration-service/handler.js
@@ -1,6 +1,6 @@
 const loadSecrets = require('./src/config/env.config.js')
 require('dotenv').config()
-//test
+//testjj
 if (process.env.ENVIRONMENT === 'production') {
   loadSecrets()
     .then(() => {
