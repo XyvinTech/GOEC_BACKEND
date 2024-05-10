0366a130befc00ca3c2df4954f6d2bfd5368fe0d
hello-world-ttj <tijotjoseph003@gmail.com>
2024-05-10 14:37:58 +0530

    #server logs

--- logs-service/src/controllers/logController.js
+++ logs-service/src/controllers/logController.js
@@ -2,12 +2,21 @@ const createError = require('http-errors')
 const LOGS = require('../models/logSchema')
 const logSchema = require('../validation/logSchema')
 
-exports.getLogs= async (req, res) =>{
-  const logData = await LOGS.find({})
-    if (!logData) {
+exports.getLogs = async (req, res) => {
+  const { pageNo, searchQuery } = req.query
+
+  const filter = {}
+
+  if (searchQuery) {
+    filter.$or = [{ label: { $regex: searchQuery, $options: 'i' } }]
+  }
+  const totalCount = await LOGS.find(filter).countDocuments();
+  const logData = await LOGS.find(filter)
+    .skip(10 * (pageNo - 1))
+    .limit(10)
+  if (!logData) {
     res.status(404).json({ error: 'Log not found' })
   } else {
-    res.status(200).json(logData)
+    res.status(200).json({ status: true, message: 'OK', result: logData, totalCount })
   }
 }
-
