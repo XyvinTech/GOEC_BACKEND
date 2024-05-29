8e4fc6103a7522db5b51014ea3f7c40058ad8bdd
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-05-29 17:32:38 +0530

    testt

--- ev-machine-service/src/app.js
+++ ev-machine-service/src/app.js
@@ -22,7 +22,7 @@ app.use(cors(
 
 app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({ extended: true , limit:"20kb" }))
-// app.use(express.static("public") only if we have any docs
+// app.use(express.static("public") only if we have any docss
 
 app.use(cookieParser())
 
