596dee0d0ce70a4b7e29cfccd6f7acdd1598ab68
hello-world-ttj <tijotjoseph003@gmail.com>
2024-06-20 17:11:38 +0530

    #Authorized

--- ocpp-service/src/controllers/ocppControllers/authorization.js
+++ ocpp-service/src/controllers/ocppControllers/authorization.js
@@ -1,50 +1,55 @@
-const { authenticateUserByRFID, authenticateUserByUserId } = require('../../services/user-service-api')
-const saveLogs = require('../../utils/saveLogs')
-let expire;
-async function handleAuthorization({ params, identity }) {
+const { authenticateUserByRFID, authenticateUserByUserId } = require('../../services/user-service-api');
+const saveLogs = require('../../utils/saveLogs');
+
+async function handleAuthorization({ params, identity, res }) {
     let messageType = 'Authorization';
+    let expire;
     try {
         expire = await saveLogs(identity, messageType, params);
-
     } catch (error) {
-        res.status(400).json({ status: false, message: `Internal Server Error ${error.message}` })
+        return res.status(400).json({ status: false, message: `Internal Server Error ${error.message}` });
     }
 
     const idTag = params.idTag;
     let isAuthorized = false;
     try {
-
-        // we have set up user unique id as alphanumeric less than 11 character
-        // and serial number  greater than 10 to differentiate.
-        if (idTag.length == 10) {
-
-            isAuthorized = await authenticateUserByUserId(idTag)
-
+        // We have set up user unique id as alphanumeric less than 11 characters
+        // and serial number greater than 10 to differentiate.
+        if (idTag.length === 10) {
+            isAuthorized = await authenticateUserByUserId(idTag);
         } else {
-            isAuthorized = await authenticateUserByRFID(idTag)
-
+            isAuthorized = await authenticateUserByRFID(idTag);
         }
     } catch (error) {
         console.log(error);
     }
 
+    const expiryDate = new Date(new Date(expire.createdAt).getTime() + 60 * 60 * 1000); // Adding 1 hour to createdAt
+    let data;
 
     if (isAuthorized) {
-        const expiryDate = new Date(new Date(expire.createdAt).getTime() + 60 * 60 * 1000);
-        return {
+        data = {
             idTagInfo: {
                 status: "Accepted",
                 expiryDate: expiryDate.toISOString(),
             },
         };
     } else {
-        return {
+        data = {
             idTagInfo: {
                 status: "Blocked",
             },
         };
     }
+
+    // Always save AuthorizationConfirmation log
+    try {
+        await saveLogs(identity, "AuthorizationConfirmation", data, "CMS");
+    } catch (error) {
+        console.log(`Error saving AuthorizationConfirmation log: ${error.message}`);
+    }
+
+    return data;
 }
 
-
-module.exports = { handleAuthorization }
+module.exports = { handleAuthorization };
