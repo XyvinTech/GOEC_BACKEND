93a4f054cfa408735b7ea90727e9d516db858070
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-07-02 12:52:31 +0530

    ocpp metervalue stop issue

--- ocpp-service/src/utils/updateMeter.js
+++ ocpp-service/src/utils/updateMeter.js
@@ -10,7 +10,7 @@ async function updateMeterAmount(transactionId, meterValue, actionType, currentS
   //get transaction details from db, which contains user, chargingTariff
   const transactionData = await OCPPTransaction.findOne({ transactionId })
   if (!transactionData) throw new Error(`Transaction with id ${transactionId} not found`)
-  const lastMeterValue = transactionData.meterStart/1000 // changed for correct calculation
+  const lastMeterValue = transactionData.lastMeterValue // changed for correct calculation
   const { user, chargingTariff } = transactionData
 
   // if (meterValue < lastMeterValue) throw new Error(`Last meter value(${lastMeterValue}) greater than current meter value(${meterValue})`)
@@ -20,17 +20,24 @@ async function updateMeterAmount(transactionId, meterValue, actionType, currentS
   if (totalAmount && actionType === "meterValues") {
     const { status, walletAmount } = await reduceMoneyFromWallet(user, totalAmount, energyConsumed)
     // if (!status) throw new Error('User Wallet could not be updated')
-    if (!status) await remoteStopTransactionFunction(transactionData.cpid, transactionData.transactionId)
+    if (!status) {
+      await remoteStopTransactionFunction(transactionData.cpid, transactionData.transactionId)
+       return //!need to test
+
+    }
     else {
       userWalletUpdated = true
       await updateWalletTransaction(transactionData.user, totalAmount, transactionData.transactionId)
     }
   }
-
+ 
   //If user wallet is not updated due to any issues, the transaction shouldn't be updated as well
-  if (!userWalletUpdated) return
-
-  let updateBody = { lastMeterValue: meterValue, }
+  // if (!userWalletUpdated) return
+  let updateBody =   {};
+  if (meterValue > lastMeterValue) {
+     updateBody.lastMeterValue = meterValue ;
+  }
+  
   if (transactionData.transaction_status != "Completed") updateBody.transaction_status = "Progress" //if transaction is already completed, don't update transaction_status
 
   if (currentSoc) updateBody.currentSoc = currentSoc
